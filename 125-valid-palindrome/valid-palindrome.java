38d3a745df8fae8b86b546e9990000eb2820d873
raviteja101 <raviteja910@gmail.com>
2024-04-21 19:11:50 +0530

    Time: 4 ms (56.88%) | Memory: 43 MB (66.32%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+           int i = 0;
+        int j = s.length()-1;
+        if(j == 0) {
+            return true;
+        }
+        s = s.toLowerCase();
+        while(i < j) {
+            if(!Character.isDigit(s.charAt(i)) && !Character.isLetter(s.charAt(i))) {
+                i++;
+                continue;
+            }
+            if(!Character.isDigit(s.charAt(j)) && !Character.isLetter(s.charAt(j))) {
+                j--;
+                continue;
+            }
+            if(s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;    
+    }
+}
