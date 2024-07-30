754a0181b9052d514a54d2ebabc865ed268c4073
raviteja101 <raviteja910@gmail.com>
2024-07-30 12:03:03 +0530

    Time: 1 ms (35.76%) | Memory: 45.1 MB (22.00%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -14,15 +14,37 @@
  * }
  */
 class Solution {
+
+
+
     public int diameterOfBinaryTree(TreeNode root) {
         if(root == null) {
             return 0;
         }
-        int lheight = heightOfTree(root.left);
-        int rheight = heightOfTree(root.right);
-        int ldiameter = diameterOfBinaryTree(root.left);
-        int rdiameter = diameterOfBinaryTree(root.right);
-        return Math.max(Math.max(ldiameter, rdiameter), lheight+rheight);
+        // int lheight = heightOfTree(root.left);
+        // int rheight = heightOfTree(root.right);
+        // int ldiameter = diameterOfBinaryTree(root.left);
+        // int rdiameter = diameterOfBinaryTree(root.right);
+        // return Math.max(Math.max(ldiameter, rdiameter), lheight+rheight);
+        
+        NodeInfo result = getDiamaterOfTree(root);
+        return result.diameter;
+    }
+
+    public NodeInfo getDiamaterOfTree(TreeNode root) {
+
+        if(root == null) {
+            return new NodeInfo();
+        }
+
+        NodeInfo left = getDiamaterOfTree(root.left);
+        NodeInfo right = getDiamaterOfTree(root.right);
+
+        int diameter = Math.max(Math.max(left.diameter, right.diameter), left.height+right.height);
+        int height = 1 + Math.max(left.height, right.height);
+
+        return new NodeInfo(height, diameter);
+
     }
 
     int heightOfTree(TreeNode root) {
@@ -30,5 +52,21 @@ class Solution {
             return 0;
         }
         return 1 + Math.max(heightOfTree(root.left), heightOfTree(root.right));
+    }
+}
+
+
+class NodeInfo {
+    int height;
+    int diameter;
+
+    NodeInfo() {
+        height = 0;
+        diameter = 0;
+    }
+
+    NodeInfo(int height, int diameter) {
+        this.height = height;
+        this.diameter = diameter;
     }
 }
